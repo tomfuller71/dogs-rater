3c4f50711bd99f37c975f2a0b233fc0c828d59d9
tfBosLA35 <67017587+tomfuller71@users.noreply.github.com>
2022-02-02 11:51:22 -0500

    Skeleton for newDogForm
    Co-authored-by: Steven <ssavitt12@users.noreply.github.com>

---
+++ client/src/components/layout/NewDogForm.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+
+const NewDogForm
+
+
+
+// return (
+//   <div className="header">
+//     <h1>Add a New Dog</h1>
+//     /* <form onSubmit */
+//       <label>
+//       Name:
+//       <
+// )
+
+export default NewDogForm
